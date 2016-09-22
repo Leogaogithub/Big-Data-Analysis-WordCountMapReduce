58e45213050a5512655d03ceb49b3387a3e722f3
Leogaogithub <longhaogao@gmail.com>
2016-09-22 13:23:17 -0500

    first commit

---
+++ src/main/java/WordCount/WordCount/WordCount.java
@@ -0,0 +1,141 @@
+package WordCount.WordCount;
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.net.URI;
+import java.util.HashSet;
+import java.util.Set;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Pattern;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.util.StringUtils;
+
+import org.apache.log4j.Logger;
+
+public class WordCount extends Configured implements Tool {
+
+  private static final Logger LOG = Logger.getLogger(WordCount.class);
+
+  public static void main(String[] args) throws Exception {
+    int res = ToolRunner.run(new WordCount(), args);
+    System.exit(res);
+  }
+
+  public int run(String[] args) throws Exception {
+    Job job = Job.getInstance(getConf(), "wordcount");
+    for (int i = 0; i < args.length; i += 1) {
+      if ("-skip".equals(args[i])) {
+        job.getConfiguration().setBoolean("wordcount.skip.patterns", true);
+        i += 1;
+        job.getConfiguration().set("wordcount.skip.patterns.filename", args[i]);
+        //job.addCacheFile(new Path(args[i]).toUri());       
+        // this demonstrates logging
+        LOG.info("Added file to the distributed cache: " + args[i]);
+      }
+    }
+    job.setJarByClass(this.getClass());
+    // Use TextInputFormat, the default unless job.setInputFormatClass is used
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    job.setMapperClass(Map.class);
+    job.setCombinerClass(Reduce.class);
+    job.setReducerClass(Reduce.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    return job.waitForCompletion(true) ? 0 : 1;
+  }
+
+  
+  public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+    private boolean caseSensitive = false;
+    private long numRecords = 0;
+    private String input;
+    private Set<String> patternsToSkip = new HashSet<String>();
+    String fileName = "";
+    private static final Pattern WORD_BOUNDARY = Pattern.compile("\\s*\\b\\s*");
+
+    protected void setup(Mapper.Context context)
+        throws IOException,
+        InterruptedException {
+      if (context.getInputSplit() instanceof FileSplit) {
+        this.input = ((FileSplit) context.getInputSplit()).getPath().toString();
+      } else {
+        this.input = context.getInputSplit().toString();
+      }
+      Configuration config = context.getConfiguration();
+      this.caseSensitive = config.getBoolean("wordcount.case.sensitive", false);
+      if (config.getBoolean("wordcount.skip.patterns", false)) {
+        //URI[] localPaths = context.getCacheFiles();
+    	String filename = config.get("wordcount.skip.patterns.filename");  
+        parseSkipFile(new Path(filename).toUri(), context);
+      }
+      System.out.println("Added file to the distributed cache2: ");
+    }
+
+    private void parseSkipFile(URI patternsURI, Mapper.Context context) {
+      LOG.info("Added file to the distributed cache2: " + patternsURI);
+      try {
+    	FileSystem fs = FileSystem.get(patternsURI, context.getConfiguration());
+        BufferedReader fis = new BufferedReader(new InputStreamReader(fs.open(new Path(patternsURI.getPath()))));
+        String pattern;
+        while ((pattern = fis.readLine()) != null) {
+          patternsToSkip.add(pattern);
+        }
+      } catch (IOException ioe) {
+        System.err.println("Caught exception while parsing the cached file '"
+            + patternsURI + "' : " + StringUtils.stringifyException(ioe));
+      }
+    }
+    
+   
+
+    public void map(LongWritable offset, Text lineText, Context context)
+        throws IOException, InterruptedException {
+      
+      String line = lineText.toString();
+      if (!caseSensitive) {
+        line = line.toLowerCase();
+      }
+      Text currentWord = new Text();     
+      for (String word : WORD_BOUNDARY.split(line)) {    	
+        if (word.isEmpty() ||patternsToSkip.contains(word)) {
+            continue;
+        }    	
+        currentWord = new Text(word);
+        context.write(currentWord,one);              
+      }
+    }
+  }
+
+  public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+    @Override
+    public void reduce(Text word, Iterable<IntWritable> counts, Context context)
+        throws IOException, InterruptedException {
+      int sum = 0;
+      for (IntWritable count : counts) {
+        sum += count.get();
+      }
+      context.write(word, new IntWritable(sum));
+    }
+  }
+}
+
